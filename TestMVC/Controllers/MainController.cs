5d8ae208b54d435b130a15c13efb06057ae7baa6
AlexSolari <alexsanify@gmail.com>
2015-05-05 16:39:28 +0300

    Forgot to save and commit

--- TestMVC/Controllers/MainController.cs
+++ TestMVC/Controllers/MainController.cs
@@ -15,26 +15,5 @@ namespace TestMVC.Controllers
         {
             return View();
         }
-        
-        public ActionResult List()
-        {
-            var events = ListModel.getEventList();
-            if (events.Count == 0)
-            {
-                events.Add(new Event("first", "simple event", "details and other stuff", "location of event"));
-                events.Add(new Event("митинг", "показать проект", "встречаемся и показываем какой у нас шикарный проект", "ИСМ"));
-                events.Add(new Event("курсач", "сдать курсач", "рили надо сдать", "универ"));
-                events.Add(new Event("тест", "тесттест", "тесттесттест", "место"));
-                events.Add(new Event("1", "2", "3", "4"));
-            }
-            ViewBag.Title = "Index";
-            return View(events);
-        }
-        [HttpGet]
-        public ActionResult Details(int id)
-        {
-            Event e = ListModel.getByID(id);
-            return View(e);
-        }
     }
 }
