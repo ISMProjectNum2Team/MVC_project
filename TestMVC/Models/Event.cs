427366c2f617417ca798a939eb6f6a13bda55b8e
Arsendiabolikkys <diabolikkys@gmail.com>
2015-05-06 19:32:11 +0300

    new folder
    Removed the field names from the model, "support" folder for the
    auxiliary classes

---
+++ TestMVC/Models/Event.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class Event
+    {
+        public string ID
+        {
+            get;
+            private set;
+        }
+
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public string Description
+        {
+            get;
+            private set;
+        }
+
+        public string Details
+        {
+            get;
+            private set;
+        }
+
+        public string Location
+        {
+            get;
+            private set;
+        }
+
+        public DateTime DateFrom
+        {
+            get;
+            private set;
+        }
+
+        public DateTime DateTo
+        {
+            get;
+            private set;
+        }
+
+        public Event(string title, string description, string details, string location,
+                    DateTime dateFrom = new DateTime(), DateTime dateTo = new DateTime())
+        {
+            ID = Guid.NewGuid().ToString();
+            Title = title;
+            Description = description;
+            Location = location;
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+            Details = details;
+        }
+
+    }
+}
---
+++ TestMVC/Models/Support/EventDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestMVC.Models
+{ 
+    public class EventDataProvider
+    {
+        public static List<Event> Data = new List<Event>() 
+        {
+                new Event("first", "simple event", "details and other stuff", "location of event"),
+                new Event("митинг", "показать проект", "встречаемся и показываем какой у нас шикарный проект", "ИСМ"),
+                new Event("курсач", "сдать курсач", "рили надо сдать", "универ"),
+                new Event("тест", "тесттест", "тесттесттест", "место"),
+                new Event("1", "2", "3", "4")
+        };
+
+        public EventDataProvider() { }
+        public List<Event> GetEventList()
+        { 
+            return Data; 
+        }
+        public Event GetByID(string id)
+        {
+            return Data.Find(e => e.ID == id);
+        }
+    }
+}
